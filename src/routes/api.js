0c89d4e53d1bcc89ff885e918fa52e705e3645e1
Stephen Kempisty <stephen.kempisty@gmail.com>
2019-08-25 11:17:49 -0700

    data pull working! clean up also

--- src/routes/api.js
+++ src/routes/api.js
@@ -16,6 +16,9 @@ router.get('/pull', async (req, res) => {
     const page = await browser.newPage();
     await page.goto(url);
 
+    // May need to bypass slider captcha
+    // https://medium.com/@filipvitas/how-to-bypass-slider-captcha-with-js-and-puppeteer-cd5e28105e3c
+
     await page.waitForSelector('table#data', { timeout: 10000});
 
     const html = await page.content();
@@ -25,19 +28,12 @@ router.get('/pull', async (req, res) => {
 
     $('table#data tbody tr').each(function(i, elem) {
         horses.push({
-            // name: $(this).children('td.horse a:first-child').text(),
-            // winPct: $(this).children('td.winpct a:first-child').text()
-            name: i,
-            winPct: elem
+            name: $(this).find('td.horse a:first-child').text(),
+            winPct: $(this).find('td.winpct').text()
         });
     });
 
     console.log("HORSES", horses);
-
-
-    // const result = await page.evaluate(() => {
-    //     //...
-    // });
 
     // put horse data model into mongo
     // Horse.create(horses)
@@ -48,34 +44,5 @@ router.get('/pull', async (req, res) => {
 
     res.send("OK");
 });
-
-
-// router.get('/todos', (req, res, next) => {
-//
-//     res.send("Hello World");
-//
-//     //this will return all the data, exposing only the id and action field to the client
-//     Todo.find({}, 'action')
-//         .then(data => res.json(data))
-//         .catch(next)
-// });
-
-// router.post('/todos', (req, res, next) => {
-//     if(req.body.action){
-//         Todo.create(req.body)
-//             .then(data => res.json(data))
-//             .catch(next)
-//     }else {
-//         res.json({
-//             error: "The input field is empty"
-//         })
-//     }
-// });
-//
-// router.delete('/todos/:id', (req, res, next) => {
-//     Todo.findOneAndDelete({"_id": req.params.id})
-//         .then(data => res.json(data))
-//         .catch(next)
-// })
 
 module.exports = router;
