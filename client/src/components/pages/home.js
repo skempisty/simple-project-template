871b5e706604b4373e7d714af1e19b5216a9fc9e
Stephen Kempisty <stephen.kempisty@gmail.com>
2020-03-02 00:03:16 -0800

    update home page to accomodate passing from and to years to backend

--- client/src/components/pages/home.js
+++ client/src/components/pages/home.js
@@ -6,7 +6,11 @@ export default class Home extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { horsesYear: '2020' };
+        this.state = {
+            horsesYear: '2020',
+            horsesFromYear: '2020',
+            horsesToYear: '2020'
+        };
     }
 
     async crawlHorses() {
@@ -17,12 +21,43 @@ export default class Home extends React.Component {
     }
 
     async crawlAllRaces() {
-        const races = await fetch(`${Constants.baseUrl}/api/races/scrape`);
+        const { horsesFromYear, horsesToYear } = this.state;
+
+        const races = await fetch(`${Constants.baseUrl}/api/races/scrape?horsesFromYear=${horsesFromYear}&horsesToYear=${horsesToYear}`);
         console.log(races);
     }
 
+    /**
+     * Returns the years that horse/race data is available for on equibase
+     *
+     * @returns {number[]}
+     */
+    get equibaseRecordYears() {
+        return [
+            2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012,2011,
+            2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001,
+            2000
+        ];
+    }
+
+    get availableFromYears() {
+        const { horsesToYear } = this.state;
+
+        return this.equibaseRecordYears.filter((year) => {
+           return year <= horsesToYear;
+        });
+    }
+
+    get availableToYears() {
+        const { horsesFromYear } = this.state;
+
+        return this.equibaseRecordYears.filter((year) => {
+            return year >= horsesFromYear;
+        });
+    }
+
     render() {
-        const { horsesYear } = this.state;
+        const { horsesYear, horsesFromYear, horsesToYear } = this.state;
 
         return (
             <div style={{
@@ -43,76 +78,70 @@ export default class Home extends React.Component {
                 }} />
 
                 <div style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
                     marginTop: '1em'
                 }}>
-                    <div style={{
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                        alignItems: 'center'
-                    }}>
-                        <div style={{
-                            display: 'flex',
-                            justifyContent: 'flex-end',
-                            width: '8em'
-                        }}>
-                            <div>
-                                Horses:
-                            </div>
-                            <select
-                                value={horsesYear}
-                                onChange={(e) => this.setState({ horsesYear: e.target.value })}
-                            >
-                                <option value="2020">2020</option>
-                                <option value="2019">2019</option>
-                                <option value="2018">2018</option>
-                                <option value="2017">2017</option>
-                                <option value="2016">2016</option>
-                                <option value="2015">2015</option>
-                                <option value="2014">2014</option>
-                                <option value="2013">2013</option>
-                                <option value="2012">2012</option>
-                                <option value="2011">2011</option>
-                                <option value="2010">2010</option>
-                                <option value="2009">2009</option>
-                                <option value="2008">2008</option>
-                                <option value="2007">2007</option>
-                                <option value="2006">2006</option>
-                                <option value="2005">2005</option>
-                                <option value="2004">2004</option>
-                                <option value="2003">2003</option>
-                                <option value="2002">2002</option>
-                                <option value="2001">2001</option>
-                                <option value="2000">2000</option>
-                            </select>
-                        </div>
+                    <div style={{ fontWeight: 'bold' }}>Horses</div>
+
+                    <div style={{ display: 'flex', marginTop: '0.5em' }}>
+                        <span>Year:</span>
+                        <select
+                            value={horsesYear}
+                            onChange={(e) => this.setState({ horsesYear: e.target.value })}
+                        >
+                            {this.equibaseRecordYears.map((year) =>
+                                <option value={year}>{year}</option>
+                            )}
+                        </select>
 
                         <button
-                            style={{ marginLeft: '1em' }}
+                            style={{ marginLeft: '1em', cursor: 'pointer', boxShadow: '1px 1px 5px grey' }}
                             onClick={() => this.crawlHorses()}
-                        >Crawl</button>
+                        >
+                            Crawl
+                        </button>
                     </div>
+                </div>
 
-                    <div style={{
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                        marginTop: '1em'
-                    }}>
-                        <div style={{
-                            display: 'flex',
-                            justifyContent: 'flex-end',
-                            width: '8em'
-                        }}>
-                            <div>
-                                Races:
-                            </div>
-                        </div>
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginTop: '1em'
+                }}>
+                    <div style={{ fontWeight: 'bold' }}>Races</div>
 
+                    <div style={{ display: 'flex', marginTop: '0.5em' }}>
+                        <span>From year:</span>
+                        <select
+                            value={horsesFromYear}
+                            onChange={(e) => this.setState({ horsesFromYear: e.target.value })}
+                        >
+                            {this.availableFromYears.map((year) =>
+                                <option value={year}>{year}</option>
+                            )}
+                        </select>
+
+                        <span style={{ marginLeft: '1em' }}>To year:</span>
+                        <select
+                            value={horsesToYear}
+                            onChange={(e) => this.setState({ horsesToYear: e.target.value })}
+                        >
+                            {this.availableToYears.map((year) =>
+                                <option value={year}>{year}</option>
+                            )}
+                        </select>
 
                         <button
-                            style={{ marginLeft: '1em' }}
+                            style={{ marginLeft: '1em', cursor: 'pointer', boxShadow: '1px 1px 5px grey' }}
                             onClick={() => this.crawlAllRaces()}
-                        >Crawl</button>
+                        >
+                            Crawl
+                        </button>
                     </div>
                 </div>
             </div>
